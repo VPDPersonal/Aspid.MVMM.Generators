0db829464799348deec560067f13fa43abae17d8
Vladislav Panin <vpd-2000@yandex.com>
2024-07-05 23:27:49 +0300

    Add Bind Inheritors Also Generator

--- MVVMGenerators/MVVMGenerators.Sample/SomeBinder.cs
+++ MVVMGenerators/MVVMGenerators.Sample/SomeBinder.cs
@@ -1,33 +1,39 @@
 using System;
 using UltimateUI.MVVM;
+using UltimateUI.MVVM.ViewModels;
 
 namespace MVVMGenerators.Sample;
 
-public partial class SomeBinder : SomeBinderParent, IBinder<string>
+public partial class SomeBinder : SomeBinderParent, IBinder<string>, IBinder<object>
 {
     [BinderLog]
     public void SetValue(string value)
     {
         throw new NotImplementedException();
     }
+
+    [BindInheritorsAlso]
+    public void SetValue(object value)
+    {
+        
+    }
 }
 
-public partial class SomeBinderParent : INumberBinder
+public partial class SomeBinderParent : INumberBinder, IBinder<IViewModel>
 {
     [BinderLog]
     public void SetValue(int value)
     {
     }
-
-    // [BindInheritorsAlso]
-    // public void SetValue(long value)
-    // {
-    //     
-    // }
     
     [BinderLog]
-    [BindInheritorsAlso]
     public void SetValue(long value)
+    {
+        
+    }
+
+    [BindInheritorsAlso]
+    public void SetValue(IViewModel value)
     {
         
     }
--- MVVMGenerators/MVVMGenerators/Descriptions/Classes.System.cs
+++ MVVMGenerators/MVVMGenerators/Descriptions/Classes.System.cs
@@ -5,6 +5,9 @@ namespace MVVMGenerators.Descriptions;
 public static partial class Classes
 {
     public static readonly TypeText Action = new("Action", Namespaces.System);
+    public static readonly TypeText Delegate = new("Delegate", Namespaces.System);
     public static readonly TypeText Exception = new("Exception", Namespaces.System);
+    
     public static readonly TypeText List = new("List", Namespaces.System_Collections_Generic);
+    public static readonly TypeText Dictionary = new("Dictionary", Namespaces.System_Collections_Generic);
 }
